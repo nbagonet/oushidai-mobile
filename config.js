ad09ef0c47840ee0d3cc122e342b64bcd09fd843
王硕 <101840192@qq.com>
2016-08-01 17:11:38 +0800

    test

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   build: {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '../static',
-    assetsPublicPath: './',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '',
     productionSourceMap: false
   },
   dev: {
