2e2cc3177818247062519715b77f50954d90cb1b
王硕 <101840192@qq.com>
2016-08-02 15:43:57 +0800

    fixed assetsPublicPath.

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     index: path.resolve(__dirname, '../../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '//nbagonet.github.io/oushidai-mobile',
+    assetsPublicPath: '//nbagonet.github.io/oushidai-mobile/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
